f3e33d01f0f87fa747b9e9d5151df471edf65679
TAYYAB119120 <l1f24bscs0572@ucp.edu.pk>
2025-03-13 22:59:00 +0500

    my 2d array project

---
+++ 2d array linear search.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+	string name;
+	string names[] = { "ali","hamza" };
+	string numbers[] = { "03450522036","031157738656" };
+	cin >> name;
+	for (int i = 0; i < 2; i++)
+	{
+		if (names[i]==name)
+		{
+			cout <<  "found"<<numbers[i];
+			
+			return 0;
+			//break;use or not
+		}
+	}
+	cout << "not found ";
+	//return 1; use or not
+}
